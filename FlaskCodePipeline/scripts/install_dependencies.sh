7ef914c2b823efa66a72de87d98ec992d2ee3634
Tim <tim.schaefer24@hotmail.de>
2023-08-09 15:24:53 +0200

    Update install_dependencies.sh

--- FlaskCodePipeline/scripts/install_dependencies.sh
+++ FlaskCodePipeline/scripts/install_dependencies.sh
@@ -2,6 +2,4 @@
 yum -y update 
 yum -y install docker
 service docker start
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin ACCOUNT_ID_TO_BE_REPLACED.dkr.ecr.eu-central-1.amazonaws.com
-docker pull ACCOUNT_ID_TO_BE_REPLACED.dkr.ecr.eu-central-1.amazonaws.com/flaskwebserver:latest
 
