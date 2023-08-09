ad4cd608579d96b362ab891a46920b10429ef216
Tim <tim.schaefer24@hotmail.de>
2023-08-09 15:24:04 +0200

    Update start_server.sh

--- FlaskCodePipeline/scripts/start_server.sh
+++ FlaskCodePipeline/scripts/start_server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin ACCOUNT_ID_TO_BE_REPLACED.dkr.ecr.eu-central-1.amazonaws.com
+docker pull ACCOUNT_ID_TO_BE_REPLACED.dkr.ecr.eu-central-1.amazonaws.com/flaskwebserver:latest
 docker run -d -t -p 80:5000 ACCOUNT_ID_TO_BE_REPLACED.dkr.ecr.eu-central-1.amazonaws.com/flaskwebserver
